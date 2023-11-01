36eab1f2d171ad14c1244b14cf0b4212e10dd89c
manuelscl <manuelscardenaslopez@gmail.com>
2023-10-31 22:22:50 -0600

    Creando un contador de clicks con React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Boton from './componentes/Boton.js';
+import Contador from './componentes/Contador.js'
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
 
 function App() {
+
+  const [contador, setContador] = useState(0);
+
+  const aumentarContador = () => {
+    return setContador((c) => c + 1);
+  };
+
+  const reducirContador = () => {
+    return setContador((c) => c - 1);
+  };
+
+  const reiniciarClic = () => {
+    return setContador(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+          className='freecodecamp-logo'
+          src={freeCodeCampLogo}
+          alt='Logo de freeCodeCamp'
+        />
+      </div>
+      <div className='contenedor-principal'>
+        <Contador numClics={contador}/>
+        <div className='contenedor-btn-clics'>
+          <Boton 
+            texto='-'
+            esBotonDeClic={true}
+            manejarClic={reducirContador}
+          />
+          <Boton 
+            texto='+'
+            esBotonDeClic={true}
+            manejarClic={aumentarContador}
+          />
+        </div>
+        <Boton 
+          texto='Reiniciar'
+          esBotonDeClic={false}
+          manejarClic={reiniciarClic}
+        />
+      </div>
     </div>
   );
 }
